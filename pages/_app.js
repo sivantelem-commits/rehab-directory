38bf28d9eda9b22da996199f23dbf185b83cdaac
sivantelem-commits <sivantelem@gmail.com>
2026-03-12 11:20:10 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,22 @@
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <img
+        src="/logo.png"
+        alt="לוגו"
+        style={{
+          position: 'fixed',
+          top: 12,
+          right: 16,
+          width: 44,
+          height: 44,
+          objectFit: 'contain',
+          mixBlendMode: 'multiply',
+          zIndex: 9999,
+          pointerEvents: 'none',
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
