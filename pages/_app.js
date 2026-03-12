3b87a5f70e2408d4d8b13d3d40870c8ec931f9af
sivantelem-commits <sivantelem@gmail.com>
2026-03-12 12:00:50 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,30 @@
+import { useRouter } from 'next/router'
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+  const isHome = router.pathname === '/'
+
+  return (
+    <>
+      {!isHome && (
+        <img
+          src="/logo.png"
+          alt="לוגו"
+          style={{
+            position: 'fixed',
+            top: 12,
+            right: 16,
+            width: 44,
+            height: 44,
+            objectFit: 'contain',
+            filter: 'brightness(0) invert(1)',
+            mixBlendMode: 'multiply',
+            zIndex: 9999,
+            pointerEvents: 'none',
+          }}
+        />
+      )}
+      <Component {...pageProps} />
+    </>
+  )
 }
