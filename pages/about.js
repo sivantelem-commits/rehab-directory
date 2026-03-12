d33534e8d06a66cbc432439ff1ee4dbfc37226cc
sivantelem-commits <sivantelem@gmail.com>
2026-03-12 14:56:22 +0200

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 
-const NAV = [['/', '🏠 ראשי'], ['/rehab', '♿ שיקום'], ['/treatment', '🏥 טיפול'], ['/map', '🗺️ מפה'], ['/register-treatment', 'הרשמת שירות'], ['/about', 'אודות'], ['/admin', 'ניהול']]
+const NAV = [['/', '🏠 ראשי'], ['/rehab', '♿ שיקום'], ['/treatment', '🏥 טיפול'], ['/map', '🗺️ מפה'], ['/register', 'הרשמת שירות'], ['/about', 'אודות'], ['/admin', 'ניהול']]
 export default function About() {
   return (
     <>
