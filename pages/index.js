36123b36a5f3e352536d52eba848c7fd280761ec
sivantelem-commits <sivantelem@gmail.com>
2026-03-12 11:10:51 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -24,8 +24,8 @@ export default function Home() {
       }}>
 
         {/* כותרת */}
-        <div style={{ textAlign: 'center', marginBottom: 52 }}>
-<img src="/logo.png" alt="לוגו" style={{ width: 320, height: 320, objectFit: 'contain', marginBottom: -40, mixBlendMode: 'multiply' }} />          <h1 style={{
+<div style={{ textAlign: 'center', marginBottom: 52, marginTop: -60 }}>
+  <img src="/logo.png" alt="לוגו" style={{ width: 320, height: 320, objectFit: 'contain', marginBottom: -40, mixBlendMode: 'multiply' }} />          <h1 style={{
             fontSize: 32, fontWeight: 800,
             color: '#3d2a6e', margin: '0 0 10px',
             letterSpacing: '-0.3px',
