b9545c9ac9700e9a97e200c6a0d9659d96078920
sivantelem-commits <sivantelem@gmail.com>
2026-03-22 09:56:25 +0200

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'bnf-v1'
+const STATIC = ['/', '/rehab', '/treatment', '/map', '/guide', '/about', '/contact', '/manifest.json', '/icon-192.png', '/icon-512.png', '/logo.png']
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(STATIC)).then(() => self.skipWaiting()))
+})
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()))
+})
+
+self.addEventListener('fetch', e => {
+  // API תמיד מהרשת
+  if (e.request.url.includes('/api/')) return
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone()
+        caches.open(CACHE).then(c => c.put(e.request, clone))
+        return res
+      })
+      .catch(() => caches.match(e.request))
+  )
+})
