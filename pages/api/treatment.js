5161232214b101f1ed14a857489f388baaa04c03
sivantelem-commits <sivantelem@gmail.com>
2026-03-23 09:50:00 +0200

    Update treatment.js

--- pages/api/treatment.js
+++ pages/api/treatment.js
@@ -38,7 +38,8 @@ export default async function handler(req, res) {
   }
 
   if (population) {
-    query = query.contains('populations', [population])
+    const pops = Array.isArray(population) ? population : [population]
+    pops.forEach(p => { query = query.contains('populations', [p]) })
   }
 
   const { data, error } = await query.order('created_at', { ascending: false })
