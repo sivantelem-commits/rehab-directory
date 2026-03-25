57503892932a28525af6833303fb4b7973742e17
sivantelem-commits <sivantelem@gmail.com>
2026-03-25 23:03:48 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml',
+        destination: '/api/sitemap',
+      },
+      {
+        source: '/robots.txt',
+        destination: '/api/robots',
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
